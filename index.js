8224e1f64aca6db1b7c112049f91600a4ea739eb
unknown <kevinserrano0823@gmail.com>
2023-10-17 19:36:02 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+const inquirer = require(inquirer);
+const fs = require(fs);
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter up to three characters:"
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter text color keyword (or a hexadecimal number):"
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Select shape for logo:",
+        choices: ["Circle", "Square", "Triangle"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter shape color keyword (or a hexadecimal number):"
+    }
+];
