86237aa411a91eb7220e5656b24faef4b605d6c6
unknown <kevinserrano0823@gmail.com>
2023-10-17 19:33:32 -0500

    .lib/

---
+++ .lib/square.js
@@ -0,0 +1,12 @@
+class square {
+    constructor(text, textColor, shape, shapeColor) {
+        this.text = "text";
+        this.textColor = "textColor";
+        this.shape = "shape";
+        shapeColor = "shapeColor";
+    }
+    render() {
+        return ``
+    }
+}
+module.exports = Square
---
+++ .lib/triangle.js
@@ -0,0 +1,12 @@
+class triangle {
+    constructor(text, textColor, shape, shapeColor) {
+        this.text = "text";
+        this.textColor = "textColor";
+        this.shape = "shape";
+        this.shapeColor = "shapeColor";
+    }
+    render() {
+        return ``
+    }
+}
+module.exports = Triangle
