a10613a61ed14005359540199825c0d8f7be1097
unknown <kevinserrano0823@gmail.com>
2023-10-17 23:29:41 -0500

    .lib/square.js

--- .lib/square.js
+++ .lib/square.js
@@ -1,12 +1,12 @@
-class square {
+class Square {
     constructor(text, textColor, shape, shapeColor) {
-        this.text = "text";
-        this.textColor = "textColor";
-        this.shape = "shape";
-        shapeColor = "shapeColor";
+        this.text = text;
+        this.textColor = textColor;
+        this.shape = shape;
+        this.shapeColor = shapeColor;
     }
     render() {
-        return ``
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><${this.shape} x="90" y="40" width="120" height="120" fill="${this.shapeColor}"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
     }
 }
 module.exports = Square
